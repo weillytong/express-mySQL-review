e560449b35d38ae4c180cac5fcb15542e3d84469
Jonathan Lim <jonathanlim035@gmail.com>
2021-02-02 18:32:07 -0800

    Added some DB solution code.

---
+++ database/index.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'gamesDB'
+});
+
+db.connect();
+
+db.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+
+db.end();
+
+module.exports = db;
